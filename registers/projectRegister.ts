1ea631ff24eacd4bd05aefbcc35b7213280797a3
Glenn Slear <glennstreetman@gmail.com>
2022-02-17 11:22:59 -0800

    register projects

--- registers/projectRegister.ts
+++ registers/projectRegister.ts
@@ -19,7 +19,7 @@ interface allProjects {
 export const projectRegister: allProjects = {
     FinnDash: {
         id: "FinnDash",
-        project: "FinnDash",
+        project: "App: FinnDash",
         description:
             "A single page web application for designing dashboards, building & sharing dataset with GraphQL, and Excel Templating using Finnhub.io API data. The front end uses React with Redux and is styled with MaterialUi. The back end is deployed using Docker-Compose. HTTP requests are handled by Express. Account data is saved in Postgres. Caching is handled by MongoDB. Datasets can be shared using GraphQL. Testing is performed using Jest & Testing Library. Hosted on AWS EC2.",
         repo: "https://github.com/GlennStreetman/finnHub-Dashboard",
@@ -28,7 +28,7 @@ export const projectRegister: allProjects = {
     },
     copyExcelChart: {
         id: "copyExcelChart",
-        project: "copyExcelChart",
+        project: "NPM Package: Copy-Excel-Chart",
         description:
             "An NPN Package used to copy charts between excel spreadsheets using NodeJS. Uses AdmZip to unpack Excel templates into source XML files. xml2js is then used to find and copy excel charts into destination files.",
         repo: "https://github.com/GlennStreetman/copyExcelChart",
@@ -36,9 +36,15 @@ export const projectRegister: allProjects = {
     },
     resumeBlogGenerator: {
         id: "resumeBlogGenerator",
-        project: "resumeBlogGenerator",
+        project: "App: Personal Blog",
         description: "Place holder gibberish",
         repo: "https:github.com/GlennStreetman/Blog",
+        languages: "language-javascript,language-Next,language-Tailwind",
+    },
+    SQLReporting: {
+        id: "SQLReporting",
+        project: "Past Work: Financial system SQL Reporting",
+        description: "Lessons from Building SQL Reports for Financial Systems",
         languages: "language-javascript,language-Next,language-Tailwind",
     },
 };
