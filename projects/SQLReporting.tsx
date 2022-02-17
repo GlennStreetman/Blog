e78ec7bb88c22131c87f6b29118dac8704f11839
Glenn Slear <glennstreetman@gmail.com>
2022-02-17 11:26:35 -0800

    add project

---
+++ projects/SQLReporting.tsx
@@ -0,0 +1,42 @@
+/*
+    @start
+    id: SQLReporting
+    project: SQL Reporting
+    description: Writing Financial Reports using SQL
+    repo: https://github.com/GlennStreetman/Blog
+    languages: language-typescript,language-Next,language-Tailwind
+    @end
+*/
+
+import CodeBlock from "../components/codeBlock";
+
+const body = function () {
+    return (
+        <div className="article">
+            <h1>Description</h1>
+            <p>PLACE HOLDER TEXT</p>
+            <h1>Technologies:</h1>
+
+            <h2>PLACE HOLDER TEXT: </h2>
+            <p>PLACE HOLDER TEXT</p>
+        </div>
+    );
+};
+
+export default body;
+
+//Problem
+
+//Approach
+
+//Solution
+
+//Accomplishments.
+
+// debt compliance tracker written with Excel VBA & an access backend
+//entity valuation stack
+//investor notes report
+//investor equity
+//investor distributions
+//Rental Valuation & Debt Coverage Report
+//collapsed chart of accounts
