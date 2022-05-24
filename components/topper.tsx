e4fc60955ff34c8759cf918aba47dbaa75d35f3e
glenn <glennstreetman@gmail.com>
2022-05-24 03:14:01 +0000

    clean up old imports

--- components/topper.tsx
+++ components/topper.tsx
@@ -1,12 +1,9 @@
 import dynamic from "next/dynamic";
 import React from "react";
-// import Gutter from "./gutter";
 import Tippy from "@tippyjs/react";
 import "tippy.js/dist/tippy.css"; // optional
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { BiLogInCircle } from "react-icons/bi";
-// import { faPencilAlt } from "@fortawesome/free-solid-svg-icons";
 import BackButton from "./backButton";
 import { useRouter } from "next/router";
 import { faUserEdit } from "@fortawesome/free-solid-svg-icons";
@@ -19,7 +16,7 @@ function Topper() {
 
     return (
         <div className="top-0  right-3 xl:right-12 fixed flex justify-end w-screen sm:w-auto col-span-12 md:col-span-10 gap-2 p-2">
-            {router.pathname !== "/" ? <BackButton hide={true} /> : <></>}
+            {router.pathname !== "/" ? <BackButton /> : <></>}
             <Tippy content="View/Modify Theme" interactive={true} interactiveBorder={20} delay={100} arrow={true}>
                 {router.pathname === "/" ? (
                     <div>
