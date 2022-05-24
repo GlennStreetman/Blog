1f4ef39cfb6e7090221671c81a1b34067dc8934b
glenn <glennstreetman@gmail.com>
2022-05-24 19:43:45 +0000

    min height updated so Bottom component sticks to bottom

--- components/bottom.tsx
+++ components/bottom.tsx
@@ -1,24 +1,17 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { VscGithub } from "react-icons/vsc";
 import React from "react";
 
 function Bottom() {
     return (
         <>
-            <div className="h-10" />
-            <div className="bottom-0 fixed w-screen h-10 bg-primary">
-                <div className="flex col-span-12 md:col-span-10 space-x-3 p-2">
-                    <div className="text-primary ">
-                        <a href="https://github.com/GlennStreetman" target="_blank">
-                            <FontAwesomeIcon className="h-7 w-7 text-primary hover:text-accent" icon={faGithub} />
-                        </a>
-                    </div>
-                    <div className="text-xs sm:text-base ">
-                        <a href="mailto:glennstreetman@gmail.com" className="text-accent">
-                            glenn@gstreet.dev
-                        </a>
-                    </div>
-                </div>
+            <div className="h-14" />
+            <div className="absolute bottom-2 left-2 flex gap-2 p-2 bg-primary mx-4 my-2 grow">
+                <a className="text-primary " href="https://github.com/GlennStreetman" target="_blank">
+                    <VscGithub className="h-7 w-7 text-primary hover:text-accent" />
+                </a>
+                <a className="text-xs sm:text-base text-accent" href="mailto:glennstreetman@gmail.com">
+                    glenn@gstreet.dev
+                </a>
             </div>
         </>
     );
