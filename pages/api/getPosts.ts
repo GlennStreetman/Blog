dfd619295076ab01fbaa2872960a11f690b90813
glenn <glennstreetman@gmail.com>
2022-05-24 21:59:20 +0000

    promisify return to clear did not complete request error

--- pages/api/getPosts.ts
+++ pages/api/getPosts.ts
@@ -3,47 +3,53 @@ import Client from "pg";
 import format from "pg-format";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-    let db = new Client.Client({
-        sslmode: "disable",
-        user: process.env.pguser,
-        host: process.env.pghost,
-        database: process.env.pgdatabase,
-        password: process.env.pgpassword,
-        port: process.env.pgporInternal,
-    });
-    const post = format(req.query.post);
+    return new Promise<void>(async (resolve, reject) => {
+        let db = new Client.Client({
+            sslmode: "disable",
+            user: process.env.pguser,
+            host: process.env.pghost,
+            database: process.env.pgdatabase,
+            password: process.env.pgpassword,
+            port: process.env.pgporInternal,
+        });
+        const post = format(req.query.post);
 
-    try {
-        const postCommentQuery = `
+        try {
+            const postCommentQuery = `
         SELECT id, userid, usermessage 
         FROM userposts
         WHERE post = '${post}'
         ORDER BY id
         `;
 
-        await db.connect();
-        await db.query(postCommentQuery, (err, rows) => {
-            if (err) {
-                console.log("problem getting comments for post", err);
-                db.end();
-                res.status(500).json({ message: "problem getting comments" });
-            } else {
-                const posts = {};
-                for (const row in rows.rows) {
-                    const post = rows.rows[row];
-                    const userID = post["userid"].slice(0, post["userid"].search("@")) ? post["userid"].slice(0, post["userid"].search("@")) : post["userid"];
-                    posts[post["id"]] = {
-                        userid: userID,
-                        message: post["usermessage"],
-                    };
+            await db.connect();
+            await db.query(postCommentQuery, (err, rows) => {
+                if (err) {
+                    console.log("problem getting comments for post", err);
+                    db.end();
+                    res.status(500).json({ message: "problem getting comments" });
+                } else {
+                    const posts = {};
+                    for (const row in rows.rows) {
+                        const post = rows.rows[row];
+                        const userID = post["userid"].slice(0, post["userid"].search("@"))
+                            ? post["userid"].slice(0, post["userid"].search("@"))
+                            : post["userid"];
+                        posts[post["id"]] = {
+                            userid: userID,
+                            message: post["usermessage"],
+                        };
+                    }
+                    db.end();
+                    res.status(200).json({ message: "success", posts });
+                    resolve();
                 }
-                db.end();
-                res.status(200).json({ message: "success", posts });
-            }
-        });
-    } catch (err) {
-        db.end();
-        console.log("Problem getting message", err);
-        res.status(500).json({ message: "failed to retrieve comments" });
-    }
+            });
+        } catch (err) {
+            db.end();
+            console.log("Problem getting message", err);
+            res.status(500).json({ message: "failed to retrieve comments" });
+            resolve();
+        }
+    });
 }
