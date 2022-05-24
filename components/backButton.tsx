4d41fef5c75bee4b6b46e5fbfb509d96e60d1d22
glenn <glennstreetman@gmail.com>
2022-05-24 03:13:28 +0000

    back button routes to /

--- components/backButton.tsx
+++ components/backButton.tsx
@@ -1,33 +1,20 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-// import { ArrowCircleLeftIcon } from "@heroicons/react/solid";
 import { faArrowAltCircleLeft } from "@fortawesome/free-solid-svg-icons";
 import { useRouter } from "next/router";
 
-interface props {
-    hide?: boolean;
-}
-
-function BackButton(p: props) {
+function BackButton() {
     const router = useRouter();
 
-    const element =
-        p.hide !== true ? (
-            <button className=" flex font-bold uppercase  text-primary hover:text-accent rounded-md active:bg-strong gap-1" onClick={() => router.back()}>
+    const element = (
+        <div className="my-auto">
+            <button className=" flex font-bold uppercase  text-primary hover:text-accent rounded-md active:bg-strong gap-1" onClick={() => router.push("/")}>
                 <div className="my-auto">
-                    <FontAwesomeIcon icon={faArrowAltCircleLeft} />{" "}
+                    <FontAwesomeIcon icon={faArrowAltCircleLeft} />
                 </div>
-                <div className="my-auto">Back</div>
             </button>
-        ) : (
-            <div className="my-auto">
-                <button className=" flex font-bold uppercase  text-primary hover:text-accent rounded-md active:bg-strong gap-1" onClick={() => router.back()}>
-                    <div className="my-auto">
-                        <FontAwesomeIcon icon={faArrowAltCircleLeft} />
-                    </div>
-                </button>
-            </div>
-        );
+        </div>
+    );
 
     return element;
 }
