5397e36aa78398e16bf58664885e1d1325cda8d1
Glenn Streetman <glennstreetman@gmail.com>
2022-08-30 19:48:14 +0000

    set component relative

--- components/hoverSurface.tsx
+++ components/hoverSurface.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 function HoverSurface(p) {
-    return <div className="border-line shadow rounded-md border-2 p-2 outline-4 hover:bg-weak hover:cursor-pointer">{p.children}</div>;
+    return <div className="border-line shadow rounded-md border-2 p-2 outline-4 hover:bg-weak hover:cursor-pointer relative">{p.children}</div>;
 }
 
 export default HoverSurface;
