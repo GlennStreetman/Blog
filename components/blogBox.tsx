59159b281804086ec90cff8b1c403d8287aa92c5
Glenn Streetman <glennstreetman@gmail.com>
2022-08-31 00:39:54 +0000

    add image to post

---
+++ components/blogBox.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function BlogBox(p){
+
+    return (
+        <div className='flex flex-inline h-fit w-full '>
+            <div className='w-1/3 md:w-1/4 h-fit'></div>
+            <div className='w-2/3 md:w-2/4 h-fit'><Image width={834} height={480} objectFit='fill' layout="responsive" src="/lighthouse.JPG" alt="lighthouse score" /></div>
+            <div className='w-1/3 md:w-1/4 h-fit'></div>
+        </div>
+    )
+}
+
