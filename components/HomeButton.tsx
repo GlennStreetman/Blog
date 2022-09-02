12d31b34b9c6da3b5ca77972e7bb6be166181671
Glenn Streetman <glennstreetman@gmail.com>
2022-09-02 02:13:32 +0000

    status authenticated to use login button. Use public redirect env variable

--- components/HomeButton.tsx
+++ components/HomeButton.tsx
@@ -2,18 +2,21 @@ import React from "react";
 
 import { GoHome } from "react-icons/go";
 import { useRouter } from "next/router";
+import Tippy from "@tippyjs/react";
 
 function HomeButton(p) {
     const router = useRouter();
 
-    const element = (
-        <button className="flex font-bold uppercase text-primary hover:text-accent rounded-md active:bg-strong gap-1 " onClick={() => router.push("/")}>
-            <GoHome className="h-7 w-7 text-primary hover:text-accent bg-primary" />
-            {p.text ? <div className="m-auto">{p.text}</div> : <></>}
-        </button>
-    );
-
-    return element;
+    return (
+        <div>
+            <Tippy content="Home" interactive={true} interactiveBorder={1} delay={1} arrow={true}>
+                <button className="flex font-bold uppercase text-primary hover:text-accent rounded-md active:bg-strong gap-1 " onClick={() => router.push("/")}>
+                    <GoHome className="h-7 w-7 text-primary hover:text-accent bg-primary" />
+                    {p.text ? <div className="m-auto">{p.text}</div> : <></>}
+                </button>
+            </Tippy>
+        </div>
+    )
 }
 
 export default HomeButton;
