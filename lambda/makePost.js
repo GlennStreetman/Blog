ce3742cdd18a04784e2f81c6a50f5f7edf7e6b9a
Glenn Streetman <glennstreetman@gmail.com>
2023-04-02 15:45:06 +0000

    stash progress

---
+++ lambda/makePost.js
@@ -0,0 +1,77 @@
+const AWS = require('aws-sdk');
+
+const dynamo = new AWS.DynamoDB();
+
+exports.handler = async (event, context) => {
+    //remember to migrate all changes to cloudformation template. Code is inlined as part of template
+    let body;
+    let statusCode = '200';
+    const headers = {
+        'Content-Type': 'application/json',
+    };
+
+    try {
+
+        switch (event.httpMethod) {
+
+            case 'POST':
+                console.log('POST', event.body)
+                const parseBody = JSON.parse(event.body)
+                const date = Date.now()
+                const user = parsebody.user || false //this needs to be calced from user credentials
+                let message = parseBody?.meta || false
+                const post = parseBody?.post || false
+
+                let insMeta = ''
+                if (postMeta) {
+                    console.log("postMeta", postMeta)
+                        for(const [key,val] of Object.entries(postMeta)){
+                            insMeta = `${insMeta}, '${key}': '${val}'`
+                    }
+                }
+                let insStatement = `INSERT INTO "blogposts" value {'ipaddress': '${postIP}', 'username': '${postUser}', 'logdate': '${postDate}'${insMeta}}`
+                
+                let updMeta = ''
+                if (postMeta) {
+                    for(const [key,val] of Object.entries(postMeta)){
+                        updMeta = `${updMeta}, ${key}='${val}'`
+                    }
+                }
+                let updStatement = `UPDATE iplog SET logdate='${postDate}' ${updMeta} WHERE ipaddress='${postIP}' AND username='${postUser}'`
+
+                if(postIP && postUser) try {
+                    console.log('insert', insStatement)
+                    await dynamo.executeStatement({Statement: insStatement}).promise();
+                    body = {msg: `IP Logged: ${new Date().toISOString().replace('T', ' ').replace('Z','')}`}
+                    break;
+                } catch {
+                    console.log('update', updStatement)
+                    await dynamo.executeStatement({Statement: updStatement}).promise();
+                    body = {msg: `IP Logged: ${new Date().toISOString().replace('T', ' ').replace('Z','')}`}
+                    break;
+                }
+                break;
+            default:
+                throw new Error(`Unsupported method "${event.httpMethod}"`);
+        }
+    } catch (err) {
+        statusCode = '400';
+        console.log(err.message)
+        body = {
+            error: 'error processing request',
+            params: 'ip, userid, date',
+            ip: 'string',
+            userid: 'string',
+            date: 'string: YYYY-MM-DD',
+            msg: 'error logging IP'
+        };
+    } finally {
+        console.log('Finaly', body, statusCode)
+        body = JSON.stringify(body);
+    }
+    return {
+        statusCode,
+        body,
+        headers,
+    };
+};
