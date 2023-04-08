88c2e488f40f98be9dc081960f1dcba15946370b
Glenn Streetman <glennstreetman@gmail.com>
2023-04-08 19:36:33 +0000

    remove image optimazation. required for static site hosting

--- next.config.js
+++ next.config.js
@@ -12,4 +12,7 @@ const withMDX = require("@next/mdx")({
 module.exports = withMDX({
     // Append the default value with md extensions
     pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+    images: {
+        unoptimized: true,
+    },
 });
