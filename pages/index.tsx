514c3c6e892b3877eb075284cbaeddb59ee52b47
Glenn Streetman <glennstreetman@gmail.com>
2023-04-08 19:37:01 +0000

    setup error redirect hook

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,9 +2,13 @@ import Left_Column from "../components/left_Column";
 import Right_Column from "../components/right_Column";
 import { getSortedPostsData } from "../lib/posts";
 import { getSortedProjectData } from "../lib/projects";
-import {buildStingers} from '../lib/buildProjects'
+import { buildStingers } from '../lib/buildProjects';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react'
+import { redirect } from 'next/navigation';
 
 export async function getStaticProps() {
+
     const allPostsData = await getSortedPostsData();
     const allProjectData = await getSortedProjectData();
     const allStingers = await buildStingers()
@@ -17,7 +21,42 @@ export async function getStaticProps() {
     };
 }
 
+function parseQueryString(qs) {
+    if (qs) {
+        const qsTrim = qs.replace('/?', '')
+        const qsObj = qsTrim.split('&').reduce((prev, curr) => {
+            if (curr.includes(('='))) {
+                const currKeyPair = curr.split('=')
+                prev[currKeyPair[0]] = currKeyPair[1]
+            }
+            return prev
+        }, {})
+        return qsObj
+    } else { return false }
+}
+
 export default function Home({ allPostsData, allProjectData, allStingers }) {
+
+    const router = useRouter()
+    console.log('pathname', router.pathname, 'queryString:', router.asPath)
+    //if pathname !== '/' AND query string obj[r]=true not present, redirect WITH querystring ?r=true
+    // redirect to pathname /w ?r=true appended
+    // all pages should strip query string r=true on successfull load except index.html
+    // else
+    //load page 
+    //if q=true present, strip query string.
+
+    useEffect(() => {
+        const queryStringObj = parseQueryString(router.asPath)
+        if (router.pathname !== '/') {
+            if (queryStringObj?.['r'] !== true) {
+                redirect(router.pathname)
+            } else {
+                redirect('/')
+            }
+        }
+    }, [])
+
     return (
         <>
             <div className="grid grid-cols-12 gap-6 mb-auto text-xs sm:text-base">
